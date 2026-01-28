74bfa7419b0a1bfd4198da369f4f030cf3af789f
Aayush0603 <140516073+Aayush0603@users.noreply.github.com>
2026-01-28 18:42:46 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+import { db } from "./firebase.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+window.login = async function () {
+  const mobile = mobileInput();
+
+  if (!/^[0-9]{10}$/.test(mobile)) {
+    alert("Enter valid 10-digit number");
+    return;
+  }
+
+  // Check admin
+  const adminSnap = await getDoc(doc(db, "admins", mobile));
+  if (adminSnap.exists()) {
+    localStorage.setItem("role", "admin");
+    localStorage.setItem("mobile", mobile);
+    window.location.href = "index.html";
+    return;
+  }
+
+  // Check customer
+  const custSnap = await getDoc(doc(db, "customers", mobile));
+  if (custSnap.exists()) {
+    localStorage.setItem("role", "customer");
+    localStorage.setItem("mobile", mobile);
+    window.location.href = "request.html";
+    return;
+  }
+
+  alert("Mobile not registered");
+};
+
+function mobileInput() {
+  return document.getElementById("mobile").value.trim();
+}
